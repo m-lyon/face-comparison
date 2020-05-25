6415a08bd766a0110e14d5fd4fbb69a52e819f82
Matt Lyon <matthewlyon18@gmail.com>
2020-05-25 10:23:43 +1000

    initial commit

---
+++ face-compare/model.py
@@ -0,0 +1,273 @@
+import numpy as np
+import tensorflow as tf
+import keras.backend.tensorflow_backend as tfback
+
+from keras.layers import Conv2D, ZeroPadding2D, Activation, Input, concatenate
+from keras.models import Model
+from keras.layers.normalization import BatchNormalization
+from keras.layers.pooling import MaxPooling2D, AveragePooling2D
+from keras.layers.core import Lambda, Flatten, Dense
+
+# from weights import load_weights
+
+import cv2
+
+
+def _get_available_gpus():
+    """Get a list of available gpu devices (formatted as strings).
+
+    # Returns
+        A list of available GPU devices.
+    """
+    #global _LOCAL_DEVICES
+    if tfback._LOCAL_DEVICES is None:
+        devices = tf.config.list_logical_devices()
+        tfback._LOCAL_DEVICES = [x.name for x in devices]
+    return [x for x in tfback._LOCAL_DEVICES if 'device:gpu' in x.lower()]
+
+
+def conv2d_bn(x, layer_name, filters, kernel_size=(1, 1), strides=(1, 1), i='', epsilon=0.00001):
+    '''2D Convolutional Block with Batch normalization and ReLU activation.
+
+    Args:
+        x (tf.Tensor): Input tensor.
+        layer_name (str): Name of layer.
+        filters (int): Number of filters to apply in 1st convolutional operation.
+        kernel_size (Tuple[int, int]): Kernel size of filter to apply.
+        strides (Tuple[int, int]): Strides of filter.
+        i (str): index to append layer name, eg. 2 for conv2.
+        epsilon (float): epsilon for batch normalization
+
+    Returns:
+        tensor (tf.Tensor): Tensor with graph applied.
+    '''
+    if layer_name:
+        conv_name = f'{layer_name}_conv{i}'
+        bn_name = f'{layer_name}_bn{i}'
+    else:
+        conv_name = f'conv{i}'
+        bn_name = f'bn{i}'
+    tensor = Conv2D(filters, kernel_size, strides=strides, data_format='channels_first', name=conv_name)(x)
+    tensor = BatchNormalization(axis=1, epsilon=epsilon, name=bn_name)(tensor)
+    tensor = Activation('relu')(tensor)
+    return tensor
+
+def inception_block_4a(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_4a_3x3', 96, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_4a_3x3', 128, kernel_size=(3, 3), i='2')
+
+    # 5x5 Block
+    X_5x5 = conv2d_bn(X, 'inception_4a_5x5', 16, i='1')
+    X_5x5 = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_5x5)
+    X_5x5 = conv2d_bn(X_5x5, 'inception_4a_5x5', 32, kernel_size=(5, 5), i='2')
+
+    # Max Pooling Block
+    X_pool = MaxPooling2D(pool_size=3, strides=2, data_format='channels_first')(X)
+    X_pool = conv2d_bn(X_pool, 'inception_4a_pool', 32)
+    X_pool = ZeroPadding2D(padding=((3, 4), (3, 4)), data_format='channels_first')(X_pool)
+
+    # 1x1 Block
+    X_1x1 = conv2d_bn(X, 'inception_4a_1x1', 64)
+
+    return concatenate([X_3x3, X_5x5, X_pool, X_1x1], axis=1)
+
+
+def inception_block_4b(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_4b_3x3', 96, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_4b_3x3', 128, kernel_size=(3, 3), i='2')
+
+    # 5x5 Block
+    X_5x5 = conv2d_bn(X, 'inception_4b_5x5', 32, i='1')
+    X_5x5 = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_5x5)
+    X_5x5 = conv2d_bn(X_5x5, 'inception_4b_5x5', 64, kernel_size=(5, 5), i='2')
+
+    # Average Pooling Block
+    X_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3), data_format='channels_first')(X)
+    X_pool = conv2d_bn(X_pool, 'inception_4b_pool', 64)
+    X_pool = ZeroPadding2D(padding=(4, 4), data_format='channels_first')(X_pool)
+
+    # 1x1 Block
+    X_1x1 = conv2d_bn(X, 'inception_4b_1x1', 64)
+
+    return concatenate([X_3x3, X_5x5, X_pool, X_1x1], axis=1)
+
+
+def inception_block_4c(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_4c_3x3', 128, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_4c_3x3', 256, kernel_size=(3, 3), strides=(2, 2), i='2')
+
+    # 5x5 Block
+    X_5x5 = conv2d_bn(X, 'inception_4c_5x5', 32, i='1')
+    X_5x5 = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_5x5)
+    X_5x5 = conv2d_bn(X_5x5, 'inception_4c_5x5', 64, kernel_size=(5, 5), strides=(2, 2), i='2')
+
+    # Max Pooling Block
+    X_pool = MaxPooling2D(pool_size=3, strides=2, data_format='channels_first')(X)
+    X_pool = ZeroPadding2D(padding=((0, 1), (0, 1)), data_format='channels_first')(X_pool)
+
+    return concatenate([X_3x3, X_5x5, X_pool], axis=1)
+
+
+def inception_block_5a(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_5a_3x3', 96, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_5a_3x3', 192, kernel_size=(3, 3), i='2')
+
+    # 5x5 Block
+    X_5x5 = conv2d_bn(X, 'inception_5a_5x5', 32, i='1')
+    X_5x5 = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_5x5)
+    X_5x5 = conv2d_bn(X_5x5, 'inception_5a_5x5', 64, kernel_size=(5, 5), i='2')
+
+    # Average Pooling Block
+    X_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3), data_format='channels_first')(X)
+    X_pool = conv2d_bn(X_pool, 'inception_5a_pool', 128)
+    X_pool = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_pool)
+
+    # 1x1 Block
+    X_1x1 = conv2d_bn(X, 'inception_5a_1x1', 256)
+
+    return concatenate([X_3x3, X_5x5, X_pool, X_1x1], axis=1)
+    
+
+def inception_block_5b(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_5b_3x3', 160, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_5b_3x3', 256, kernel_size=(3, 3), strides=(2, 2), i='2')
+
+    # 5x5 Block
+    X_5x5 = conv2d_bn(X, 'inception_5b_5x5', 64, i='1')
+    X_5x5 = ZeroPadding2D(padding=(2, 2), data_format='channels_first')(X_5x5)
+    X_5x5 = conv2d_bn(X_5x5, 'inception_5b_5x5', 128, kernel_size=(5, 5), strides=(2, 2), i='2')
+
+    # Max Pooling Block
+    X_pool = MaxPooling2D(pool_size=3, strides=2, data_format='channels_first')(X)
+    X_pool = ZeroPadding2D(padding=((0, 1), (0, 1)), data_format='channels_first')(X_pool)
+
+    return concatenate([X_3x3, X_5x5, X_pool], axis=1)
+
+
+def inception_block_6a(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_6a_3x3', 96, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_6a_3x3', 384, kernel_size=(3, 3), i='2')
+
+    # Average Pooling Block
+    X_pool = AveragePooling2D(pool_size=(3, 3), strides=(3, 3), data_format='channels_first')(X)
+    X_pool = conv2d_bn(X_pool, 'inception_6a_pool', 96)
+    X_pool = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_pool)
+
+    # 1x1 Block
+    X_1x1 = conv2d_bn(X, 'inception_6a_1x1', 256)
+
+    return concatenate([X_3x3, X_pool, X_1x1], axis=1)
+
+
+def inception_block_6b(X):
+    # 3x3 Block
+    X_3x3 = conv2d_bn(X, 'inception_6b_3x3', 96, i='1')
+    X_3x3 = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_3x3)
+    X_3x3 = conv2d_bn(X_3x3, 'inception_6b_3x3', 384, kernel_size=(3, 3), i='2')
+
+    # Max Pooling Block
+    X_pool = MaxPooling2D(pool_size=3, strides=2, data_format='channels_first')(X)
+    X_pool = conv2d_bn(X_pool, 'inception_6b_pool', 96)
+    X_pool = ZeroPadding2D(padding=(1, 1), data_format='channels_first')(X_pool)
+
+    # 1x1 Block
+    X_1x1 = conv2d_bn(X, 'inception_6b_1x1', 256)
+
+    return concatenate([X_3x3, X_pool, X_1x1], axis=1)
+
+
+
+def facenet_model(input_shape):
+    '''Implementation of the Inception model used for FaceNet.
+    
+    Arguments:
+    input_shape (Tuple[int]): Shape of the images of the dataset.
+
+    Returns:
+    model (keras.models.Model): FaceNet model.
+    '''
+        
+    # Define the input as a tensor with shape input_shape
+    X_input = Input(input_shape)
+
+    # Zero-Padding
+    X = ZeroPadding2D((3, 3))(X_input)
+    
+    # First Block
+    X = conv2d_bn(X, '', 64, kernel_size=(7, 7), strides=(2, 2), i='1', epsilon=0.001)
+    
+    # Zero-Padding + MAXPOOL
+    X = ZeroPadding2D((1, 1))(X)
+    X = MaxPooling2D((3, 3), strides = 2)(X)
+    
+    # Second Block
+    X = conv2d_bn(X, '', 64, i='2')
+    
+    # Zero-Padding + MAXPOOL
+    X = ZeroPadding2D((1, 1))(X)
+
+    # Third Block
+    X = conv2d_bn(X, '', 192, kernel_size=(3, 3), i='3')
+    
+    # Zero-Padding + MAXPOOL
+    X = ZeroPadding2D((1, 1))(X)
+    X = MaxPooling2D(pool_size = 3, strides = 2)(X)
+    
+    # Fourth Block (Inception)
+    X = inception_block_4a(X)
+    X = inception_block_4b(X)
+    X = inception_block_4c(X)
+    
+    # Fifth Block (Inception)
+    X = inception_block_5a(X)
+    X = inception_block_5b(X)
+    
+    # Sixth Block (Inception)
+    X = inception_block_6a(X)
+    X = inception_block_6b(X)
+    
+    # Top layer
+    X = AveragePooling2D(pool_size=(3, 3), strides=(1, 1), data_format='channels_first')(X)
+    X = Flatten()(X)
+    X = Dense(128, name='dense')(X)
+    
+    # L2 normalization
+    X = Lambda(lambda  x: tfback.l2_normalize(x,axis=1))(X)
+
+    # Create model instance
+    model = Model(inputs = X_input, outputs = X, name='FaceNetModel')
+        
+    return model
+
+if __name__ == '__main__':
+    img = cv2.imread('/Users/matt/Dev/git/facenet-test/facenet-face-recognition/images/mutty.jpg')
+    image = cv2.resize(img, (96, 96))
+    img = image[...,::-1]
+    img = np.around(np.transpose(img, (2,0,1))/255.0, decimals=12)
+    x_train = np.array([img])
+
+    tfback._get_available_gpus = _get_available_gpus
+    tfback._get_available_gpus()
+    tfback.set_image_data_format('channels_first')
+
+    model = facenet_model(input_shape=(3, 96, 96))
+
+    weight_fpath = '/Users/matt/Dev/git/face-comparison/face-compare/facenet_weights.h5'
+    model.load_weights(weight_fpath)
+
+    embedding = model.predict_on_batch(x_train)
+    print(embedding)
+
+    
---
+++ face-compare/weights/weights.py
@@ -0,0 +1,134 @@
+import numpy as np
+import tensorflow as tf
+from pathlib import Path
+
+
+WEIGHTS = [
+  'conv1', 'bn1', 'conv2', 'bn2', 'conv3', 'bn3',
+  'inception_3a_1x1_conv', 'inception_3a_1x1_bn',
+  'inception_3a_pool_conv', 'inception_3a_pool_bn',
+  'inception_3a_5x5_conv1', 'inception_3a_5x5_conv2', 'inception_3a_5x5_bn1', 'inception_3a_5x5_bn2',
+  'inception_3a_3x3_conv1', 'inception_3a_3x3_conv2', 'inception_3a_3x3_bn1', 'inception_3a_3x3_bn2',
+  'inception_3b_3x3_conv1', 'inception_3b_3x3_conv2', 'inception_3b_3x3_bn1', 'inception_3b_3x3_bn2',
+  'inception_3b_5x5_conv1', 'inception_3b_5x5_conv2', 'inception_3b_5x5_bn1', 'inception_3b_5x5_bn2',
+  'inception_3b_pool_conv', 'inception_3b_pool_bn',
+  'inception_3b_1x1_conv', 'inception_3b_1x1_bn',
+  'inception_3c_3x3_conv1', 'inception_3c_3x3_conv2', 'inception_3c_3x3_bn1', 'inception_3c_3x3_bn2',
+  'inception_3c_5x5_conv1', 'inception_3c_5x5_conv2', 'inception_3c_5x5_bn1', 'inception_3c_5x5_bn2',
+  'inception_4a_3x3_conv1', 'inception_4a_3x3_conv2', 'inception_4a_3x3_bn1', 'inception_4a_3x3_bn2',
+  'inception_4a_5x5_conv1', 'inception_4a_5x5_conv2', 'inception_4a_5x5_bn1', 'inception_4a_5x5_bn2',
+  'inception_4a_pool_conv', 'inception_4a_pool_bn',
+  'inception_4a_1x1_conv', 'inception_4a_1x1_bn',
+  'inception_4e_3x3_conv1', 'inception_4e_3x3_conv2', 'inception_4e_3x3_bn1', 'inception_4e_3x3_bn2',
+  'inception_4e_5x5_conv1', 'inception_4e_5x5_conv2', 'inception_4e_5x5_bn1', 'inception_4e_5x5_bn2',
+  'inception_5a_3x3_conv1', 'inception_5a_3x3_conv2', 'inception_5a_3x3_bn1', 'inception_5a_3x3_bn2',
+  'inception_5a_pool_conv', 'inception_5a_pool_bn',
+  'inception_5a_1x1_conv', 'inception_5a_1x1_bn',
+  'inception_5b_3x3_conv1', 'inception_5b_3x3_conv2', 'inception_5b_3x3_bn1', 'inception_5b_3x3_bn2',
+  'inception_5b_pool_conv', 'inception_5b_pool_bn',
+  'inception_5b_1x1_conv', 'inception_5b_1x1_bn',
+  'dense'
+]
+
+SHAPES = {
+  'conv1': [64, 3, 7, 7],
+  'conv2': [64, 64, 1, 1],
+  'conv3': [192, 64, 3, 3],
+  'inception_3a_1x1_conv': [64, 192, 1, 1],
+  'inception_3a_pool_conv': [32, 192, 1, 1],
+  'inception_3a_5x5_conv1': [16, 192, 1, 1],
+  'inception_3a_5x5_conv2': [32, 16, 5, 5],
+  'inception_3a_3x3_conv1': [96, 192, 1, 1],
+  'inception_3a_3x3_conv2': [128, 96, 3, 3],
+  'inception_3b_3x3_conv1': [96, 256, 1, 1],
+  'inception_3b_3x3_conv2': [128, 96, 3, 3],
+  'inception_3b_5x5_conv1': [32, 256, 1, 1],
+  'inception_3b_5x5_conv2': [64, 32, 5, 5],
+  'inception_3b_pool_conv': [64, 256, 1, 1],
+  'inception_3b_1x1_conv': [64, 256, 1, 1],
+  'inception_3c_3x3_conv1': [128, 320, 1, 1],
+  'inception_3c_3x3_conv2': [256, 128, 3, 3],
+  'inception_3c_5x5_conv1': [32, 320, 1, 1],
+  'inception_3c_5x5_conv2': [64, 32, 5, 5],
+  'inception_4a_3x3_conv1': [96, 640, 1, 1],
+  'inception_4a_3x3_conv2': [192, 96, 3, 3],
+  'inception_4a_5x5_conv1': [32, 640, 1, 1,],
+  'inception_4a_5x5_conv2': [64, 32, 5, 5],
+  'inception_4a_pool_conv': [128, 640, 1, 1],
+  'inception_4a_1x1_conv': [256, 640, 1, 1],
+  'inception_4e_3x3_conv1': [160, 640, 1, 1],
+  'inception_4e_3x3_conv2': [256, 160, 3, 3],
+  'inception_4e_5x5_conv1': [64, 640, 1, 1],
+  'inception_4e_5x5_conv2': [128, 64, 5, 5],
+  'inception_5a_3x3_conv1': [96, 1024, 1, 1],
+  'inception_5a_3x3_conv2': [384, 96, 3, 3],
+  'inception_5a_pool_conv': [96, 1024, 1, 1],
+  'inception_5a_1x1_conv': [256, 1024, 1, 1],
+  'inception_5b_3x3_conv1': [96, 736, 1, 1],
+  'inception_5b_3x3_conv2': [384, 96, 3, 3],
+  'inception_5b_pool_conv': [96, 736, 1, 1],
+  'inception_5b_1x1_conv': [256, 736, 1, 1],
+  'dense': (128, 736)
+}
+
+def load_weights(model):
+    '''Loads weights to given FaceNet model
+    
+    Args:
+        model (keras.models.Models): FaceNet model.
+    '''
+
+    weights_dir = Path(__file__).parent.joinpath('weights')
+
+    for layer_name in WEIGHTS:
+        print(f'loading layer {layer_name}')
+        if 'conv' in layer_name:
+            model.get_layer(layer_name).set_weights(get_conv_weights(weights_dir, layer_name))
+        elif 'bn' in layer_name:
+            model.get_layer(layer_name).set_weights(get_batch_norm_weights(weights_dir, layer_name))
+        elif 'dense' in layer_name:
+            model.get_layer(layer_name).set_weights(get_dense_weights(weights_dir, layer_name))
+    
+
+def get_conv_weights(weights_dir, layer_name):
+    '''Loads convolutional layer's weights and biases
+        given the root weight directory, and the layer name.
+    '''
+    weight_path = weights_dir.joinpath(f'{layer_name}_w.csv')
+    conv_weight = np.genfromtxt(weight_path, delimiter=',', dtype=None)
+    conv_weight = np.reshape(conv_weight, SHAPES[layer_name])
+    conv_weight = np.transpose(conv_weight, (2, 3, 1, 0))
+
+    bias_path = weights_dir.joinpath(f'{layer_name}_b.csv')
+    conv_bias = np.genfromtxt(bias_path, delimiter=',', dtype=None)
+
+    return [conv_weight, conv_bias]
+
+def get_batch_norm_weights(weights_dir, layer_name):
+    '''Loads batch normalization weights from file'''
+    bn_weight = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_w.csv'), delimiter=',', dtype=None
+    )
+    bn_bias = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_b.csv'), delimiter=',', dtype=None
+    )
+    bn_mean = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_m.csv'), delimiter=',', dtype=None
+    )
+    bn_var = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_v.csv'), delimiter=',', dtype=None
+    )
+    return [bn_weight, bn_bias, bn_mean, bn_var]
+    
+def get_dense_weights(weights_dir, layer_name):
+    '''Loads dense layer weights from file'''
+    dense_weight = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_w.csv'), delimiter=',', dtype=None
+    )
+    dense_weight = np.reshape(dense_weight, SHAPES[layer_name])
+    dense_weight = np.transpose(dense_weight, (1, 0))
+    dense_bias = np.genfromtxt(
+        weights_dir.joinpath(f'{layer_name}_b.csv'), delimiter=',', dtype=None
+    )
+    return [dense_weight, dense_bias]
+    
